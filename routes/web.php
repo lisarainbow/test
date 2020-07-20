b6411d1e7f5dad16cbd5d6d5b7a9f83831b3dd69
lisarainbow <lisarainbow85@gmail.com>
2020-07-20 16:45:28 +0300

    change routes

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 
+Route::get('/home', 'HomeController@index');
+
 Route::prefix('user')->group(function() {
 
     Route::post('eat', 'UsersController@eatUsers')->name('eatUsers');
